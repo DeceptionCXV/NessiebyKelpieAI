89570f633afd1a400ed4a9568eafa248d7b993af
DeceptionCXV <56640056+DeceptionCXV@users.noreply.github.com>
2025-12-03 08:13:26 +0000

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
-import { Router } from './components/Router';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { LoginPage } from './pages/LoginPage';
+import { ProtectedRoute } from './components/ProtectedRoute';
+import { NessieQueue } from './pages/NessieQueue';
+import { CreateBatchPage } from './pages/CreateBatchPage';
+// ... other imports
 
 function App() {
-  return <Router />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Public route */}
+        <Route path="/login" element={<LoginPage />} />
+        
+        {/* Protected routes */}
+        <Route 
+          path="/queue" 
+          element={
+            <ProtectedRoute>
+              <NessieQueue />
+            </ProtectedRoute>
+          } 
+        />
+        
+        <Route 
+          path="/queue/new" 
+          element={
+            <ProtectedRoute>
+              <CreateBatchPage />
+            </ProtectedRoute>
+          } 
+        />
+        
+        {/* Redirect root to queue */}
+        <Route path="/" element={<Navigate to="/queue" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
