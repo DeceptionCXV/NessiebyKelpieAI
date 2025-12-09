f8af589f881be596bcff6259118cbe86fb50e3ef
DeceptionCXV <56640056+DeceptionCXV@users.noreply.github.com>
2025-12-09 07:40:22 +0000

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,44 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { LoginPage } from './pages/LoginPage';
-import { ProtectedRoute } from './components/ProtectedRoute';
-import { NessieQueue } from './pages/NessieQueue';
-import { CreateBatchPage } from './pages/CreateBatchPage';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { LoginPage } from "./pages/LoginPage";
+import { ProtectedRoute } from "./components/ProtectedRoute";
+import { NessieQueue } from "./pages/NessieQueue";
+import { CreateBatchPage } from "./pages/CreateBatchPage";
 import { NessieStatusBar } from "./components/NessieStatusBar";
 // ... other imports
 
 function App() {
   return (
     <BrowserRouter>
+      {/* All your routes */}
       <Routes>
         {/* Public route */}
         <Route path="/login" element={<LoginPage />} />
-        
+
         {/* Protected routes */}
-        <Route 
-          path="/queue" 
+        <Route
+          path="/queue"
           element={
             <ProtectedRoute>
               <NessieQueue />
             </ProtectedRoute>
-          } 
+          }
         />
-        
-        <Route 
-          path="/queue/new" 
+
+        <Route
+          path="/queue/new"
           element={
             <ProtectedRoute>
               <CreateBatchPage />
             </ProtectedRoute>
-          } 
+          }
         />
-        
+
         {/* Redirect root to queue */}
         <Route path="/" element={<Navigate to="/queue" replace />} />
       </Routes>
+
+      {/* Persistent footer – shown on ALL pages */}
+      <NessieStatusBar />
     </BrowserRouter>
   );
 }
