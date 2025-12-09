8f90ac8b2b5ede1ae256f54ad24f8d1c0b8cc183
DeceptionCXV <56640056+DeceptionCXV@users.noreply.github.com>
2025-12-09 08:02:59 +0000

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,51 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { LoginPage } from "./pages/LoginPage";
-import { ProtectedRoute } from "./components/ProtectedRoute";
-import { NessieQueue } from "./pages/NessieQueue";
-import { CreateBatchPage } from "./pages/CreateBatchPage";
-import { NessieStatusBar } from "./components/NessieStatusBar";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { LoginPage } from './pages/LoginPage';
+import { ProtectedRoute } from './components/ProtectedRoute';
+import { NessieQueue } from './pages/NessieQueue';
+import { CreateBatchPage } from './pages/CreateBatchPage';
+import { SettingsPage } from './pages/SettingsPage';
+import { NessieStatusBar } from './components/NessieStatusBar';
 // ... other imports
 
 function App() {
   return (
     <BrowserRouter>
-      {/* All your routes */}
       <Routes>
         {/* Public route */}
         <Route path="/login" element={<LoginPage />} />
-
+        
         {/* Protected routes */}
-        <Route
-          path="/queue"
+        <Route 
+          path="/queue" 
           element={
             <ProtectedRoute>
               <NessieQueue />
             </ProtectedRoute>
-          }
+          } 
         />
-
-        <Route
-          path="/queue/new"
+        
+        <Route 
+          path="/queue/new" 
           element={
             <ProtectedRoute>
               <CreateBatchPage />
             </ProtectedRoute>
-          }
+          } 
         />
-
+        
+        <Route 
+          path="/settings" 
+          element={
+            <ProtectedRoute>
+              <SettingsPage />
+            </ProtectedRoute>
+          } 
+        />
+        
         {/* Redirect root to queue */}
         <Route path="/" element={<Navigate to="/queue" replace />} />
       </Routes>
-
+      
       {/* Persistent footer – shown on ALL pages */}
       <NessieStatusBar />
     </BrowserRouter>
